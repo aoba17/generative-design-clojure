6cc3ff900b5f1940457987ac76faf9d9a4867ae3
aoba17 <aoba.ot@gmail.com>
2021-01-10 19:25:37 +0900

    implement P_2_1_2_03

---
+++ src/generative_design_clojure/01_P/P_2_1_2_03/P_2_1_2_03.clj
@@ -0,0 +1,49 @@
+(ns generative-design-clojure.01-P.P-2-1-2-03.P-2-1-2-03
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [generative-design-clojure.util :as util]))
+
+(defn setup []
+  {:module-color    (q/color 0)
+   :module-alpha    180
+   :act-random-seed 0
+   :max-distance    500})
+
+(defn draw [{:keys [module-color
+                    module-alpha
+                    act-random-seed
+                    max-distance]}]
+  (q/background 255)
+  (q/no-fill)
+  (q/random-seed act-random-seed)
+  (q/stroke (q/red module-color)
+            (q/green module-color)
+            (q/blue module-color)
+            module-alpha)
+  (q/stroke-weight 3)
+
+  (loop [grid-y 0]
+    (when (< grid-y (q/height))
+      (loop [grid-x 0]
+        (when (< grid-x (q/width))
+          (let [diameter (*  (/  (q/dist (q/mouse-x) (q/mouse-y)
+                                         grid-x grid-y)
+                                 max-distance)
+                             40)]
+            (q/with-translation [grid-x grid-y (* diameter 5)]
+              (q/rect 0 0 diameter diameter)))
+          (recur (+ grid-x 25))))
+      (recur (+ grid-y 25)))))
+
+(defn mouse-pressed [state _]
+  (assoc state :act-random-seed (q/random 100000)))
+
+(q/defsketch P-2-1-2-03
+  :renderer :p3d
+  :middleware [m/fun-mode]
+  :size [600 600]
+  :setup setup
+  :draw draw
+  :key-pressed util/key-controller
+  :mouse-pressed mouse-pressed
+  :settings q/smooth)
