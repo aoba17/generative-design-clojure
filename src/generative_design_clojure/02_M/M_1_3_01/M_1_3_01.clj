04ffb8c08c05b8a9d35eaff3b5023ed3bad2a890
aoba17 <aoba.ot@gmail.com>
2021-08-16 03:28:16 +0900

    implement M-1-3-01

---
+++ src/generative_design_clojure/02_M/M_1_3_01/M_1_3_01.clj
@@ -0,0 +1,36 @@
+(ns generative-design-clojure.02-M.M-1-3-01.M-1-3-01
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [generative-design-clojure.util :as util]))
+
+(defn draw [_]
+  (q/background 255)
+  (q/stroke 0 130 164)
+  (q/stroke-weight 1)
+  (q/stroke-join :round)
+  (q/no-fill)
+  (let [noise-range (/ (q/mouse-x) 10)]
+    (q/begin-shape)
+    (doseq [x (->> (q/width)
+                   (range)
+                   (filter #(= (mod % 10) 0)))]
+      (let [noise-x (q/map-range x 0 (q/width) 0 noise-range)
+            y       (* (q/noise noise-x) (q/height))]
+        (q/vertex x y)))
+    (q/end-shape)))
+
+(defn mouse-released [_ _]
+  (q/noise-seed (q/random 100000)))
+
+(defn key-released [state event]
+  (case (:key event)
+    :s (util/save-frame))
+  state)
+
+(q/defsketch M-1-3-01
+  :middleware [m/fun-mode]
+  :size [1024 256]
+  :draw draw
+  :mouse-released mouse-released
+  :key-released key-released
+  :settings q/smooth)
